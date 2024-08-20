3ddb7bfb6e1c59c48659d5e670016cdfd90de79e
faahvor <tinafavour2018@gmail.com>
2024-08-20 23:49:27 +0100

    update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import data from "../assets/Data.json";
+import data from "../assets/mockData.json";
 import BlogCard from "../components/BlogCard";
 
-const Blog = () => {
-  const [blogPosts] = useState(data.blogPosts);
 
+const Blog = () => {
+  const [blogPosts] = useState(data?.blogPosts);
   return (
     <div>
       <h1>Blog</h1>
-      <div className="grid grid-cols-3 justify-items-center gap-10">
+      <div className="grid grid-cols-3 px-16 gap-10 justify-items-center py-10">
         {blogPosts.map((post) => (
           <BlogCard key={post.id} post={post} />
         ))}
       </div>
+
     </div>
   );
 };
